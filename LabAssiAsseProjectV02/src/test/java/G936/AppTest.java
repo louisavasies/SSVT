0f461d2ab7377a2c27c6306e67d7662aceab3117
Louisa Vasies <louisavasies74@gmail.com>
2019-03-18 16:53:20 +0200

    Initial commit

---
+++ LabAssiAsseProjectV02/src/test/java/G936/AppTest.java
@@ -0,0 +1,20 @@
+package G936;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
