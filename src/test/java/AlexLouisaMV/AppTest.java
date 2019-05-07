83ba55467f7a824ec26e889e22a022e56e7a000d
Louisa Vasies <louisavasies74@gmail.com>
2019-05-07 20:41:22 +0300

    Lab04

--- src/test/java/AlexLouisaMV/AppTest.java
+++ src/test/java/AlexLouisaMV/AppTest.java
@@ -2,6 +2,8 @@ package AlexLouisaMV;
 
 import static org.junit.Assert.assertTrue;
 
+import AlexLouisaMV.domain.Nota;
+import AlexLouisaMV.repository.NotaXMLRepo;
 import org.junit.Test;
 import AlexLouisaMV.curent.Curent;
 import AlexLouisaMV.domain.Student;
@@ -12,6 +14,11 @@ import AlexLouisaMV.service.Service;
 import AlexLouisaMV.validation.StudentValidator;
 import AlexLouisaMV.validation.TemaValidator;
 import AlexLouisaMV.validation.ValidationException;
+import AlexLouisaMV.validation.NotaValidator;
+
+import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.Collections;
 
 /**
  * Unit test for simple App.
@@ -140,7 +147,7 @@ public class AppTest {
     }
 
     @Test
-    public void test5() {
+    public void test5() { // this will just test the validator
         System.out.println("Test 5");
 
         // need to test : ID, descriere, deadline, primire
@@ -206,12 +213,14 @@ public class AppTest {
         try {
             service.addTema(new Tema(null, "aaa", 3, 3));
             throw new RuntimeException("This should go on the catch block boss");
-        } catch (ValidationException ignored) {}
+        } catch (ValidationException ignored) {
+        }
 
         try {
             service.addTema(new Tema("", "aaa", 3, 3));
             throw new RuntimeException("This should go on the catch block boss");
-        } catch (ValidationException ignored) {}
+        } catch (ValidationException ignored) {
+        }
         //=====================================================================================
 
         //= Subtest 2 - Fail because description is empty =====================================
@@ -220,38 +229,43 @@ public class AppTest {
         try {
             service.addTema(new Tema("aaa", "", 3, 3));
             throw new RuntimeException("This should go on the catch block boss");
-        } catch (ValidationException ignored) {}
+        } catch (ValidationException ignored) {
+        }
         //=====================================================================================
 
-        //= Subtest 3 - Fail because deadline is not good =====================================
+        //= Subtest 4 - Fail because deadline is not good =====================================
         service = new Service(null, null, temaXMLRepo, temaValidator, null, null);
 
         try {
             service.addTema(new Tema("aaa", "aaa", 0, 3));
             throw new RuntimeException("This should go on the catch block boss");
-        } catch (ValidationException ignored) {}
+        } catch (ValidationException ignored) {
+        }
 
         try {
             service.addTema(new Tema("aaa", "aaa", 15, 3));
             throw new RuntimeException("This should go on the catch block boss");
-        } catch (ValidationException ignored) {}
+        } catch (ValidationException ignored) {
+        }
         //=====================================================================================
 
-        //= Subtest 4 - Fail because primire is not good ======================================
+        //= Subtest 5 - Fail because primire is not good ======================================
         service = new Service(null, null, temaXMLRepo, temaValidator, null, null);
 
         try {
             service.addTema(new Tema("aaa", "aaa", 3, 0));
             throw new RuntimeException("This should go on the catch block boss");
-        } catch (ValidationException ignored) {}
+        } catch (ValidationException ignored) {
+        }
 
         try {
             service.addTema(new Tema("aaa", "aaa", 3, 15));
             throw new RuntimeException("This should go on the catch block boss");
-        } catch (ValidationException ignored) {}
+        } catch (ValidationException ignored) {
+        }
         //=====================================================================================
 
-        //= Subtest 5 - Success ===============================================================
+        //= Subtest 6 - Success ===============================================================
         temaXMLRepo = new TemaXMLRepo("fisiere/TestTeme_1.xml");
         service = new Service(null, null, temaXMLRepo, temaValidator, null, null);
 
@@ -275,5 +289,72 @@ public class AppTest {
 
         assert size == 1;
         //=====================================================================================
+
+        //= Subtest 7 - assignment already exits
+        service.addTema(st6_tema);
+        assert (Collections.singletonList(service.getAllTeme()).size() == 1);
+
+        service.deleteTema(st6_tema.getNrTema());
+    }
+
+    @Test
+    public void testGrade() {
+        StudentXMLRepo studentXMLRepo = new StudentXMLRepo("fisiere/TestStudenti_2.xml");
+        TemaXMLRepo temaXMLRepo = new TemaXMLRepo("fisiere/TestTeme_2.xml");
+        NotaXMLRepo notaXMLRepo = new NotaXMLRepo("fisiere/TestNote_2.xml");
+
+        StudentValidator studentValidator = new StudentValidator();
+        TemaValidator temaValidator = new TemaValidator();
+        NotaValidator notaValidator = new NotaValidator(studentXMLRepo, temaXMLRepo);
+
+        Service service = new Service(studentXMLRepo, studentValidator, temaXMLRepo, temaValidator, notaXMLRepo, notaValidator);
+
+        Student student = new Student("111", "1111", 1111, "1111");
+        service.addStudent(student);
+        assert (service.findStudent(student.getID()).equals(student));
+
+        Tema tema = new Tema("aaa", "aaa", 10, 10);
+        service.addTema(tema);
+        assert (Arrays.asList(service.getAllTeme()).size() == 1);
+
+        // Today's catch: the XML Repo will ONLY create the file once you actually write something to it. Just creating an instance of the xml repo ain't enough man!!
+
+        Nota nota = new Nota("1", student.getID(), tema.getID(), 10, LocalDate.now());
+        service.addNota(nota, "EXCELLENT");
+
+        service.deleteNota(nota.getID());
+
+        nota = new Nota("1", student.getID(), tema.getID(), 10, LocalDate.of(2012, 1, 1));
+        try {
+            service.addNota(nota, "bad");
+        } catch (ValidationException ignored) {// nothing to test here
+        }
+
+        service.deleteStudent(student.getID());
+        service.deleteTema(tema.getNrTema());
+    }
+
+    @Test
+    public void BigBang() {
+        test3();
+        test4();
+        test5();
+        test6();
+        testGrade();
+    }
+
+    @Test
+    public void Incremental() {
+        test3();
+        test4();
+    }
+
+    @Test
+    public void SecondIncremental() {
+        test3();
+        test4();
+
+        test5();
+        test6();
     }
 }
